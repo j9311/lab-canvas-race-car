d7c4704a67c7f53ef31c1e998eef6e2f525e71ce
James Post <avilapost93@gmail.com>
2021-01-28 13:25:58 -0800

    lots of updates; movement, collision, assets

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 let canvas = document.querySelector("canvas")
 let ctx = canvas.getContext("2d")
 
-// roadImg.onload = animate
+// roadImg.onload = animate //
 let roadImg = new Image()
-roadImg.src = "./images/road.png"
+roadImg.src = "./images/finalfrontier.png"
 let road = {x: 0, y: 0, w: 550, h: 700, img: roadImg}
 
-// carImg.onload = animate
-let carImg = new Image()
-carImg.src = "./images/car.png"
+// carImg.onload = animate //
+let muskImg = new Image()
+muskImg.src = "./images/elon.png"
 
-class Car {
+//mr musk//
+class Musk {
   constructor(x, y, w, h, img) {
     this.x = x
     this.y = y
@@ -22,113 +23,143 @@ class Car {
     ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
   }
 }
-
 let blockImg = new Image()
-blockImg.src = "./images/cone.jpg"
+blockImg.src = "./images/jb.png"
 
+
+let speedMultiplier = 1;
+let startSpeed = 1;
+// herr bezos//
 class Block {
   constructor(x, y, w, h, img) {
-    this.x =x
-    this.y =y
-    this.w = w  
+    this.x = x
+    this.y = y
+    this.w = w
     this.h = h
     this.img = img
   }
   draw() {
-
+    this.y += startSpeed*speedMultiplier;
     ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
+  }
+  collisionDetection(this, punishedElon)
+}
+
+//blob.assign//
+let blockOne = new Block (Math.random()*canvas.width, 0, 75, 75, blockImg)
+// let blockTwo = new Block (Math.random()*canvas.width, 0, 75, 75, blockImg)
+// let blockThree = new Block (Math.random()*canvas.width, 0, 75, 75, blockImg)
+let punishedElon = new Musk((canvas.width/2 - 150/2), (canvas.height/2 + 150), 95, 150, muskImg) 
+
+let blocks = []
+
+//working area//
+setInterval(function () {
+  console.log('check')
+  let blockOne = new Block (Math.random()*400, 0, 75, 75, blockImg)
+  blocks.push(blockOne)
+  console.log(blocks)
+}, 1000)
+
+
+function drawBezosOne() {
+  for (blockOne of blocks) {
+      blockOne.draw()
+  }
+}
+
+setInterval(function () {
+  console.log('check')
+  let blockTwo = new Block (Math.random()*400, 0, 75, 75, blockImg)
+  speedMultiplier = Math.min(speedMultiplier + .02, 3)
+  blocks.push(blockTwo)
+  console.log(blocks)
+}, 1000)
+
+
+function drawBezosTwo() {
+  for (blockTwo of blocks) {
+      blockTwo.draw()
+  }
+}
+
+setInterval(function () {
+  console.log('check')
+  let blockThree = new Block (Math.random()*400, 0, 75, 75, blockImg)
+  blocks.push(blockThree)
+  console.log(blocks)
+}, 1000)
+
+
+function drawBezosThree() {
+  for (blockThree of blocks) {
+      blockThree.draw()
+  }
+}
+
+// function movingBlock () {
+//   speed1 += 3;
+//   speed2 += 4;
+//   speed3 += 7;
+
+  
+// }
+
+
+
+function collisionDetection(punishedElon, Block) {
+
+  if (punishedElon.x < Block.x + Block.w &&
+      punishedElon.x + punishedElon.w > Block.x &&
+      punishedElon.y < Block.y + Block.h &&
+      punishedElon.y + punishedElon.h > Block.y) {
+      // collision detected!
+      alert('collision detected')
+      console.log('collision!!!')
+      cancelAnimationFrame(interval)
   }
 }
 
 
-/////////////////
-let blockOne = new Block (
-  200,
-  100,
-  50,
-  50,
-  blockImg
-)
+let interval=null
 
-let blockTwo = new Block (
-  100,
-  300,
-  50,
-  50,
-  blockImg
-)
-
-let badBlocks = [
-  blockOne, blockTwo
-]
-
-// setInterval( () => {
-//   badBlocks.push({
-//     x:200, y:100, w:50, h:50
-//   })
-// }, 2000);
-
-// function drawBlockOne() {
-//     blockOne.x += 1
-//     blockOne.y += 1
-//     blockOne += 1
-//     ctx.fillStyle = 'red'
-//     ctx.fillRect(blockOne.x, blockOne.y, blockOne.w, blockOne.h)
-// }
-
-
-// function drawBlockTwo() {
-//     blockTwo.x += 1
-//     blockTwo.y += 1
-//     blockTwo += 1
-//     ctx.fillStyle = 'black'
-//     ctx.fillRect(blockTwo.x, blockTwo.y, blockTwo.w, blockTwo.h)
-// }
-
-// function drawRectangles(){
-//   for (let block of badBlocks){
-//     block.y -= 1
-//     ctx.fillRect(block.x, block.y, block.w, block.h)
-//   }
-// }
-
-///////////////////
-
-let toyota = new Car(255, 390, 100, 200, carImg) 
-
-let interval = null;
-
-
+//engine//
 function animate() {
   let gameLoop = requestAnimationFrame(animate)
   console.log("animate")
-  
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
   ctx.drawImage(roadImg, road.x, road.y, road.w, road.h)
- ctx.drawImage(blockImg, Block.x, Block.y, Block.w, Block.h)
-  toyota.draw()
-
+  // blockOne.draw()
+  // blockTwo.draw()
+  // blockThree.draw()
+  punishedElon.draw()
+  drawBezosOne()
+  drawBezosTwo()
+  drawBezosThree()
 }
-
-
-
 animate()
 
+
+
+//controls//
 window.onkeydown = function(e) {
   console.log(e.key)
     switch (e.key) {    
       case "ArrowLeft":
-        // if (toyota.x >= 0)
-        toyota.x -= 30;
+       if (punishedElon.x >= 0)
+       punishedElon.x -= 50;
        break;
       case "ArrowRight":
-        // if ((toyota.x < canvas.width-toyota.w))
-        toyota.x += 30;
+         if ((punishedElon.x < canvas.width-punishedElon.w))
+         punishedElon.x += 50;
         break
       case "ArrowUp":
-        toyota.y -= 30;
+        if (punishedElon.y > 0)
+        punishedElon.y -= 50;
        break;
       case "ArrowDown":
-        toyota.y += 30;
+        if (punishedElon.y < canvas.height-punishedElon.h)
+        punishedElon.y += 50;
         break
     }
 }
